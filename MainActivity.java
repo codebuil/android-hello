6fe69de5c6635c5448b143d55009a756057ec81e
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-10-15 12:57:29 +0100

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -4,6 +4,7 @@ import android.app.*;
 import android.os.*;
 import android.app.AlertDialog;
 import android.app.Activity;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity 
@@ -14,14 +15,16 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        Toast toast=Toast.makeText(getApplicationContext(),"Hello Javatpoint",Toast.LENGTH_SHORT);  
+         toast.setMargin(50,50);  
+         toast.show();  
+
+         builder.setMessage("hello world");
+         builder.setTitle("hello");
 
 
-builder.setMessage("hello world");
-       builder.setTitle("hello");
-
-
-AlertDialog dialog = builder.create();
-dialog.show();
+         AlertDialog dialog = builder.create();
+        dialog.show();
 
     }
 }
